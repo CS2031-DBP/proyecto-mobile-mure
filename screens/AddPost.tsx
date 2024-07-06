1da314a0c635cbd95aede23d5379fc5d0f7cd336
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-07-06 11:22:49 -0500

    Update package.json with new dependencies and remove unused code

--- screens/AddPost.tsx
+++ screens/AddPost.tsx
@@ -12,13 +12,82 @@ import {
 	ParamListBase,
 	useNavigation,
 } from "@react-navigation/native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useImagePicker from "@/hooks/useImagePicker";
+import { Audio } from "expo-av";
 
 export default function AddPost() {
 	const navigation = useNavigation<NavigationProp<ParamListBase>>();
 	const [postText, setPostText] = useState("");
 	const imagePickerHook = useImagePicker();
+	const bottomIconsSize = 27;
+	const [recording, setRecording] = useState<Audio.Recording>();
+	const [isRecording, setIsRecording] = useState(false);
+	const [permissionResponse, requestPermission] = Audio.usePermissions();
+	const [sound, setSound] = useState<Audio.Sound>();
+	const [recordingUri, setRecordingUri] = useState<string | null | undefined>(
+		null
+	);
+
+	async function startRecording() {
+		try {
+			if (permissionResponse?.status !== "granted") {
+				console.log("Requesting permission..");
+				await requestPermission();
+			}
+
+			setIsRecording(true);
+
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: true,
+				playsInSilentModeIOS: true,
+			});
+
+			console.log("Starting recording..");
+			const { recording } = await Audio.Recording.createAsync(
+				Audio.RecordingOptionsPresets.HIGH_QUALITY
+			);
+			setRecording(recording);
+			console.log("Recording started");
+		} catch (err) {
+			console.error("Failed to start recording", err);
+		}
+	}
+
+	async function stopRecording() {
+		console.log("Stopping recording..");
+		setRecording(undefined);
+		await recording?.stopAndUnloadAsync();
+		await Audio.setAudioModeAsync({
+			allowsRecordingIOS: false,
+		});
+		const uri = recording?.getURI();
+		setIsRecording(false);
+		setRecordingUri(uri);
+		console.log("Recording stopped and stored at", uri);
+	}
+
+	async function playSound() {
+		console.log("Loading Sound");
+		console.log("Recording URI", recordingUri);
+		const { sound } = await Audio.Sound.createAsync({
+			uri: recordingUri as string,
+		});
+
+		setSound(sound);
+
+		console.log("Playing Sound");
+		await sound.playAsync();
+	}
+
+	useEffect(() => {
+		return sound
+			? () => {
+					console.log("Unloading Sound");
+					sound.unloadAsync();
+			  }
+			: undefined;
+	}, [sound]);
 
 	return (
 		<SafeAreaView
@@ -46,20 +115,41 @@ export default function AddPost() {
 			>
 				<Avatar.Icon icon="account" size={40} />
 				<TextInput
+					contentStyle={{ fontSize: 17 }}
 					mode="outlined"
 					placeholder="What's happennig?"
 					style={{ flex: 1 }}
 					onChangeText={setPostText}
+					value={postText}
 				/>
 			</View>
 			<Divider />
-			<View style={{ flexDirection: "row" }}>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-between",
+				}}
+			>
+				<View style={{ flexDirection: "row" }}>
+					<IconButton
+						icon="image"
+						size={bottomIconsSize}
+						onPress={() => imagePickerHook.pickImage()}
+					/>
+					<IconButton
+						icon={isRecording ? "stop" : "microphone"}
+						size={bottomIconsSize}
+						onPress={() => {
+							isRecording ? stopRecording() : startRecording();
+						}}
+					/>
+					<IconButton icon="music" size={bottomIconsSize} />
+				</View>
 				<IconButton
-					icon="image"
-					onPress={() => imagePickerHook.pickImage()}
+					icon="play"
+					size={bottomIconsSize}
+					onPress={() => playSound()}
 				/>
-				<IconButton icon="microphone-outline" />
-				<IconButton icon="music" />
 			</View>
 		</SafeAreaView>
 	);
