a82c8349ea291ebb1526e96537de5569d4bc539c
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-07-15 15:57:58 -0500

    Refactoring 1/2

--- src/core/navigation/RegisterStackNavigator.tsx
+++ src/core/navigation/RegisterStackNavigator.tsx
@@ -1,34 +1,34 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { defaultScreenOptions } from "./ScreenOptions";
 import RegisterDetailsScreen, {
-	RegisterDetailsProps,
-} from "@features/auth/register/RegisterDetailsScreen";
-import RegisterBasicInfoScreen from "@features/auth/register/RegisterBasicInfoScreen";
+  RegisterDetailsProps,
+} from "@features/auth/register-2/RegisterDetailsScreen";
+import RegisterBasicInfoScreen from "@features/auth/register-2/RegisterBasicInfoScreen";
 
 export type RegisterStackParamList = {
-	RegisterBasicInfoScreen: undefined;
-	RegisterDetailsScreen: RegisterDetailsProps;
+  RegisterBasicInfoScreen: undefined;
+  RegisterDetailsScreen: RegisterDetailsProps;
 };
 
 const RegisterStack = createNativeStackNavigator<RegisterStackParamList>();
 
 export default function RegisterStackNavigator() {
-	return (
-		<RegisterStack.Navigator
-			initialRouteName={"RegisterBasicInfoScreen"}
-			screenOptions={{ ...defaultScreenOptions }}
-		>
-			<RegisterStack.Screen
-				name="RegisterBasicInfoScreen"
-				component={RegisterBasicInfoScreen}
-				options={{ headerTitle: "Create account" }}
-			/>
-			<RegisterStack.Screen
-				name="RegisterDetailsScreen"
-				component={RegisterDetailsScreen}
-				options={{ headerTitle: "" }}
-				initialParams={{ email: "", birthdate: new Date() }}
-			/>
-		</RegisterStack.Navigator>
-	);
+  return (
+    <RegisterStack.Navigator
+      initialRouteName={"RegisterBasicInfoScreen"}
+      screenOptions={{ ...defaultScreenOptions }}
+    >
+      <RegisterStack.Screen
+        name="RegisterBasicInfoScreen"
+        component={RegisterBasicInfoScreen}
+        options={{ headerTitle: "Create account" }}
+      />
+      <RegisterStack.Screen
+        name="RegisterDetailsScreen"
+        component={RegisterDetailsScreen}
+        options={{ headerTitle: "" }}
+        initialParams={{ email: "", birthdate: new Date() }}
+      />
+    </RegisterStack.Navigator>
+  );
 }
