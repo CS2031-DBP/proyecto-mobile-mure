29ee0749ed48dae26e5cf5252e79f705a1a794b7
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-07-15 02:24:49 -0500

    notifications - hay un bug xd

--- App.tsx
+++ App.tsx
@@ -4,14 +4,29 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { UserProvider } from "./contexts/UserContext";
+import { UserProvider, useUserContext } from "./contexts/UserContext";
+import useNotifications from "./hooks/useNotifications";
+import NotificationDisplay from './components/NotificationDisplay';
 
+function MainApp() {
+	const { user } = useUserContext();
+	const userId = user ? user.id : 0;
+	const { notification } = useNotifications(userId);
+  
+	return (
+		<>
+			<NotificationDisplay notification={notification} />
+			<AppNavigation />
+		</>
+	);
+}
+	
 export default function App() {
 	return (
-		<UserProvider>
-			<SafeAreaProvider>
-				<AppNavigation />
-			</SafeAreaProvider>
-		</UserProvider>
+	<UserProvider>
+		<SafeAreaProvider>
+			<MainApp />
+		</SafeAreaProvider>
+	</UserProvider>
 	);
 }
